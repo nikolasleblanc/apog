b25b1f0553c02657774e099b6696311a5e995b25
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2013-08-06 21:19:02 -0400

    Add CiviCRM

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -210,7 +210,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'apog',
+      'username' => 'admin',
+      'password' => 'password',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => 'apog_',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -242,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '0mtrGFwb4hugxRkiClxG-v9CqtjsvOiMxkdTx8fw4ew';
 
 /**
  * Base URL (optional).
